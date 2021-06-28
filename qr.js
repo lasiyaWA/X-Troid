15ad1e8f943bf8f888e2574013e1667325963fbf
lasiya <84390673+lasiyaWA@users.noreply.github.com>
2021-06-28 20:06:45 +0530

    Add files via upload

---
+++ qr.js
@@ -0,0 +1,47 @@
+/* # Exclusively from danuma project 
+# Do not use this fore any commercial thing
+# If you abuse thais bot we wil kick you from bot 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
+*/
+
+const chalk = require('chalk');
+const {WAConnection} = require('@adiwajshing/baileys');
+const {StringSession} = require('./whatsasena/');
+const fs = require('fs');
+
+async function whatsAsena () {
+    const conn = new WAConnection();
+    const Session = new StringSession();  
+    conn.logger.level = 'warn';
+    conn.regenerateQRIntervalMs = 50000;
+    
+    conn.on('connecting', async () => {
+        console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
+${chalk.white.italic('AsenaString Kodu Alıcı')}
+
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
+    });
+    
+
+    conn.on('open', () => {
+        var st = Session.createStringSession(conn.base64EncodedAuthInfo());
+        console.log(
+            chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
+        );
+        
+        if (!fs.existsSync('config.env')) {
+            fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
+        }
+
+        console.log(
+            chalk.blue.bold('Locale kuruyorsanız node bot.js ile botu başlatabilirsiniz.')
+        );
+        process.exit(0);
+    });
+
+    await conn.connect();
+}
+
+whatsAsena()
