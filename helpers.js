0c615487173f752ee747499977b7cc92bdc57efd
lasiya <84390673+lasiyaWA@users.noreply.github.com>
2021-06-28 20:08:30 +0530

    Add files via upload

---
+++ helpers.js
@@ -0,0 +1,25 @@
+/*
+# Exclusively from danuma project 
+# Do not use this fore any commercial thing
+# If you abuse thais bot we wil kick you from bot 
+# Do not edit (Respect to the Devaoloper) 
+# All rights reserved ©Lasiya @lasiya99X t.me/lasiya99X
+# Get more about devaoloper https://lasiya.ml
+*/
+
+function successfullMessage(msg) {
+    return "✅ *x-Troid*:  ```" + msg + "```"
+}
+function errorMessage(msg) {
+    return "🛑 *x-Troid*:  ```" + msg + "```"
+}
+function infoMessage(msg) {
+    return "⏺️ *x-Troid*:  ```" + msg + "```"
+}
+
+
+module.exports = {
+    successfullMessage,
+    errorMessage,
+    infoMessage
+}
